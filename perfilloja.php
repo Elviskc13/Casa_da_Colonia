e0042fa6bd8338f4e94384f5ee02fbd20316b888
Elviskc13 <89864413+Elviskc13@users.noreply.github.com>
2023-12-06 20:33:55 -0300

    Add files via upload

---
+++ perfilloja.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php
+    require_once("header.php");
+    // require_once("config.php");
+?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_perfilloja.css">
+    <title>Catalogo da Loja</title>
+</head>
+<main class="main_body">
+<body>
+    <div class="container_mostruario">
+        <div class="perfil_monstruario">
+            <div class="fundo_background">
+                <div class="perfil_imagem">
+                    <img src="img/perfil.png" alt="" class="perfil_imagem">
+                    <img src="img/back.png" alt="" class="background_imagem">
+                </div>
+                <div class="texto_perfil">
+                    <p class="textos_info" id="nome_loja">Laura da Colombia KKK</p>
+                    <p class="textos_info" id="nome_proprietario">Laura Amelia de Souza</p>
+                    <p class="textos_info" id="contato_loja">51 995856322</p>
+                    <p class="textos_info" id="local_loja">Local: <span id="localPlaceholder">[Local]</span></p>
+                    <p class="textos_info" id="horario_loja">07 - 22H</p>
+                </div>
+            </div>
+        </div>
+        <div class="item_loja">
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+            <div class="produtos_imagem">
+                <img src="img/linguica.png" alt="" class="item_imagem">
+                <p class="Nome_Item_Mostruario">Linguica</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
