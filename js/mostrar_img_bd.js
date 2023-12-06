fff7635bef7cdf710d1e682352c08b7695c4f26e
Thiago Winicius <108305063+ThiagoWSilva@users.noreply.github.com>
2023-12-06 20:38:30 -0300

    coloquei js

---
+++ js/mostrar_img_bd.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/*Previewprod.js*/
+
+let photo = document.getElementById('camera_imagem');
+let file = document.getElementById('input_imagem');
+
+photo.addEventListener('click', () => {
+    file.click();
+});
+
+file.addEventListener('change', () => {
+
+    if (file.files.length <= 0) {
+        return;
+    }
+
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        photo.src = reader.result;
+    }
+
+    reader.readAsDataURL(file.files[0]);
+});
